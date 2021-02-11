16943a08bf6ad4906a6227806e02398ee344fe70
Admin <tarik@gmail.com>
2021-02-11 09:56:55 -0500

    devoir requetes-asynchrones V1

--- src/App.js
+++ src/App.js
@@ -4,34 +4,69 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import {ListGroupItem, Container, ListGroup, Col, Row} from "react-bootstrap";
 
 
 
 function App() {
-  const [data, setData] = useState('Nothing yet');
+  const [data, setData] = useState([]);
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState('welcome');
 
   const fetch = async () => {
     try {
-      const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      const res = await axios.get(`https://api.github.com/users/${username}/repos`)
       console.log(res);
-      setData(res.data.title);
+      setData(res.data);
     } catch (error) {
-      console.log(error);
+        setError('Bad username')
+        console.log('error');
     }
+  }
+
+  let content ;
+
+  if (data.length !==0){
+      content = data.map((val, id) =>
+          (
+              <Container  key={val.id}>
+                  <Row  className="justify-content-md-center">
+                      <Col md={5}>
+                          <ListGroup>
+                              <ListGroupItem>
+                                  {val.name} <Button href={val.html_url}>Repos</Button>
+                              </ListGroupItem>
+                          </ListGroup>
+                      </Col>
+                  </Row>
+              </Container>
+          )
+      )
+  } else {
+      content =  (
+        <main className="App-main">
+          <Jumbotron>
+              {error}
+          </Jumbotron>
+        </main>
+      )
   }
 
   return (
     <div className="App">
       <header className="App-header">
-          <Button variant="primary" onClick={fetch} className="mt-3 mb-3">
-            Submit
-          </Button>
+          <label style={{color:'navajowhite'}}>User Name</label>
+          <input type='text' placeholder='thisisabadusernameforreal' value={username}
+                 onChange={(event) => setUsername(event.target.value)}/>
+
+          <Button variant="primary" onClick={fetch} className="mt-3 mb-3"> Submit</Button>
+
+          <>
+          { content }
+          </>
+
       </header>
-      <main className="App-main">
-        <Jumbotron>
-          {data}
-        </Jumbotron>
-      </main>
+
     </div>
   );
 }
